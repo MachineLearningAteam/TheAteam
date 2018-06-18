40bc64fff73ffecbac126263145472c98bc9d013
TaiseiIto <taisei1212@outlook.jp>
2018-06-18 11:12:00 +0900

    不要になったコードを削除

--- src/Ant.java
+++ src/Ant.java
@@ -21,8 +21,6 @@ public class Ant {
 	//その蟻がいるセルの行番号列番号
 	private int x;
 	private int y;
-	//ひとつの食べ物または全ても食べ物を得て帰還モードに入っているかどうか.(期間モードには入らないようにする)
-	//private boolean returnToNest;
 	//その蟻がいる世界
 	Cell[][] world;
 	//巣もしくは食べ物を出発してからの経過ステップ数
@@ -49,169 +47,164 @@ public class Ant {
 		//食べ物が消去されていたらその食べ物の記憶を消去する.
 		foodFound.retainAll(ants.getFood());
 
-		//巣に帰ろうとしていない時
-		//if(!returnToNest){ //look for food
-			//そこに食べ物がある場合
-			if(world[x][y].isGoal()){
-				//自分が見つけた食べ物セルに現在地のセルを追加する.
-				foodFound.add(world[x][y]);
-				//Food NeedでAllが選択されている場合
-				if(Ant.allFoodRequired){
-					//全ての食べ物を見つけていたら帰還モードに入る.(ここを帰還モードに入らないようにする.)
-					/*
-					if(foodFound.size() >= ants.getFood().size()){
-						steps = 0;
-						returnToNest = true;
-						return;
-					}
-					*/
-				}
-				//Food NeedでOneが選択されている場合
-				else{
-					//帰還モードに入る.(ここを帰還モードに入らないようにする.)
-					/*
+		//そこに食べ物がある場合
+		if(world[x][y].isGoal()){
+			//自分が見つけた食べ物セルに現在地のセルを追加する.
+			foodFound.add(world[x][y]);
+			//Food NeedでAllが選択されている場合
+			if(Ant.allFoodRequired){
+				//全ての食べ物を見つけていたら帰還モードに入る.(ここを帰還モードに入らないようにする.)
+				/*
+				if(foodFound.size() >= ants.getFood().size()){
 					steps = 0;
 					returnToNest = true;
 					return;
-					*/
 				}
+				*/
 			}
-			//そこに食べ物がなく巣があり出発してから時間が2以上経過している場合場合
-			else if(world[x][y].hasNest()){
-				if(steps > 1){
-					//die();
-					return;
-				}
+			//Food NeedでOneが選択されている場合
+			else{
+				//帰還モードに入る.(ここを帰還モードに入らないようにする.)
+				/*
+				steps = 0;
+				returnToNest = true;
+				return;
+				*/
 			}
-			//今までに調べた隣接セルの中で最も強い食べ物フェロモン
-			double maxFoodSoFar = 0;
-			//今までに調べた隣接セルの中で最も強い巣のフェロモン
-			//double maxNestSoFar = 0;
-			//食べ物フェロモンが最も大きい隣接セルの集合
-			List<Cell> maxFoodCells = new ArrayList<Cell>();
-			//全ての隣接セル
-			List<Cell> allNeighborCells = new ArrayList<Cell>();
-			//全ての隣接セルに届いている全ての食べ物フェロモンの和
-			double totalNeighborPheromones = 0;
-			//その食べ物セルから隣接セルに届いている食べ物フェロモンの最大値
-			Map<Cell, Double> maxFoodSoFarMap = new HashMap<Cell, Double>();
-			//隣接セルをひとつずつ見ていく
-			for(int c = -1; c <=1; c++){
+		}
+		//そこに食べ物がなく巣があり出発してから時間が2以上経過している場合場合
+		else if(world[x][y].hasNest()){
+			if(steps > 1){
+				//die();
+				return;
+			}
+		}
+		//今までに調べた隣接セルの中で最も強い食べ物フェロモン
+		double maxFoodSoFar = 0;
+		//食べ物フェロモンが最も大きい隣接セルの集合
+		List<Cell> maxFoodCells = new ArrayList<Cell>();
+		//全ての隣接セル
+		List<Cell> allNeighborCells = new ArrayList<Cell>();
+		//全ての隣接セルに届いている全ての食べ物フェロモンの和
+		double totalNeighborPheromones = 0;
+		//その食べ物セルから隣接セルに届いている食べ物フェロモンの最大値
+		Map<Cell, Double> maxFoodSoFarMap = new HashMap<Cell, Double>();
+		//隣接セルをひとつずつ見ていく
+		for(int c = -1; c <=1; c++){
 
-				if(x+c < 0 || x+c >= world.length){
+			if(x+c < 0 || x+c >= world.length){
+				continue;
+			}
+
+			for(int r = -1; r <= 1; r++){
+				//don't count yourself
+				if(c == 0 && r == 0){
+					continue;
+				}
+				else if(y+r < 0 || y+r >= world[0].length){
 					continue;
 				}
 
-				for(int r = -1; r <= 1; r++){
-					//don't count yourself
-					if(c == 0 && r == 0){
-						continue;
-					}
-					else if(y+r < 0 || y+r >= world[0].length){
-						continue;
-					}
+				if(!world[x+c][y+r].isBlocked()){
+					//全ての隣接セルにその隣接セルを追加する.
+					allNeighborCells.add(world[x+c][y+r]);
 
-					if(!world[x+c][y+r].isBlocked()){
-						//全ての隣接セルにその隣接セルを追加する.
-						allNeighborCells.add(world[x+c][y+r]);
-
-						if(maxFoodSoFar == 0){
-							maxFoodCells.add(world[x+c][y+r]);
+					if(maxFoodSoFar == 0){
+						maxFoodCells.add(world[x+c][y+r]);
+					}
+					//その蟻が見つけた全ての食べ物セルについて
+					for(Cell food : foodFound){
+						//その食べ物セルから隣接セルに届いている食べ物フェロモンの最大値を更新する.
+						if(!maxFoodSoFarMap.containsKey(food) || world[x+c][y+r].getFoodPheromoneLevel(food) > maxFoodSoFarMap.get(food)){
+							maxFoodSoFarMap.put(food, world[x+c][y+r].getFoodPheromoneLevel(food));
 						}
-						//その蟻が見つけた全ての食べ物セルについて
-						for(Cell food : foodFound){
-							//その食べ物セルから隣接セルに届いている食べ物フェロモンの最大値を更新する.
-							if(!maxFoodSoFarMap.containsKey(food) || world[x+c][y+r].getFoodPheromoneLevel(food) > maxFoodSoFarMap.get(food)){
-								maxFoodSoFarMap.put(food, world[x+c][y+r].getFoodPheromoneLevel(food));
+
+					}
+					//そこに食べ物がある場合
+					if(world[x][y].isGoal()){
+						maxFoodSoFarMap.put(world[x][y], Cell.maxFoodPheromoneLevel);
+					}
+
+					for(Cell food : foodFound){
+						//蟻が食べ物フェロモンをそのセルに運んできた時に呼び出される関数.その蟻が運んできた食べ物フェロモンを自身の食べ物フェロモンマップに追加または更新する.
+						world[x][y].setFoodPheromone(food, maxFoodSoFarMap.get(food) * Ant.dropoffRate);
+					}
+					//環境中に食べ物セルがない場合
+					if(ants.getFood().isEmpty()){
+						totalNeighborPheromones += 1;
+					}
+					//環境中に食べ物セルがある場合
+					else{
+						//全ての食べ物セルについて
+						for(Cell food : ants.getFood()){
+							//その食べ物セルを自分がすでに見つけているならなにもしない.
+							if(foodFound.contains(food)){
+								continue;
 							}
-
-						}
-						//そこに食べ物がある場合
-						if(world[x][y].isGoal()){
-							maxFoodSoFarMap.put(world[x][y], Cell.maxFoodPheromoneLevel);
-						}
-
-						for(Cell food : foodFound){
-							//蟻が食べ物フェロモンをそのセルに運んできた時に呼び出される関数.その蟻が運んできた食べ物フェロモンを自身の食べ物フェロモンマップに追加または更新する.
-							world[x][y].setFoodPheromone(food, maxFoodSoFarMap.get(food) * Ant.dropoffRate);
-						}
-						//環境中に食べ物セルがない場合
-						if(ants.getFood().isEmpty()){
-							totalNeighborPheromones += 1;
-						}
-						//環境中に食べ物セルがある場合
-						else{
-							//全ての食べ物セルについて
-							for(Cell food : ants.getFood()){
-								//その食べ物セルを自分がすでに見つけているならなにもしない.
-								if(foodFound.contains(food)){
-									continue;
-								}
-								//その食べ物セルからその隣接セルに届いている食べ物フェロモンを全ての隣接セルに届いている全ての食べ物フェロモンの和に足す.
-								totalNeighborPheromones += world[x+c][y+r].getFoodPheromoneLevel(food);
-								//最大の食べ物フェロモンを更新する.
-								if(world[x+c][y+r].getFoodPheromoneLevel(food) > maxFoodSoFar){
-									maxFoodSoFar = world[x+c][y+r].getFoodPheromoneLevel(food);
-									maxFoodCells.clear();
-									maxFoodCells.add(world[x+c][y+r]);
-								}
-								else if(world[x+c][y+r].getFoodPheromoneLevel(food) == maxFoodSoFar){
-									maxFoodCells.add(world[x+c][y+r]);
-								}
-							}	
-						}
+							//その食べ物セルからその隣接セルに届いている食べ物フェロモンを全ての隣接セルに届いている全ての食べ物フェロモンの和に足す.
+							totalNeighborPheromones += world[x+c][y+r].getFoodPheromoneLevel(food);
+							//最大の食べ物フェロモンを更新する.
+							if(world[x+c][y+r].getFoodPheromoneLevel(food) > maxFoodSoFar){
+								maxFoodSoFar = world[x+c][y+r].getFoodPheromoneLevel(food);
+								maxFoodCells.clear();
+								maxFoodCells.add(world[x+c][y+r]);
+							}
+							else if(world[x+c][y+r].getFoodPheromoneLevel(food) == maxFoodSoFar){
+								maxFoodCells.add(world[x+c][y+r]);
+							}
+						}	
 					}
 				}
 			}
-			//蟻の移動先を決定する.
-			//確率Ant.bestCellNextでこっちを実行する.
-			//食べ物フェロモンが最も大きい隣接セルに移動する.
-			if(Ant.bestCellNext > chanceToTakeBest){
-				if(!maxFoodCells.isEmpty()){
-					int cellIndex = (int) (maxFoodCells.size()*Math.random());
-					Cell bestCellSoFar = maxFoodCells.get(cellIndex);
+		}
+		//蟻の移動先を決定する.
+		//確率Ant.bestCellNextでこっちを実行する.
+		//食べ物フェロモンが最も大きい隣接セルに移動する.
+		if(Ant.bestCellNext > chanceToTakeBest){
+			if(!maxFoodCells.isEmpty()){
+				int cellIndex = (int) (maxFoodCells.size()*Math.random());
+				Cell bestCellSoFar = maxFoodCells.get(cellIndex);
 
-					x = bestCellSoFar.c;
-					y = bestCellSoFar.r;
+				x = bestCellSoFar.c;
+				y = bestCellSoFar.r;
+			}
+		}	
+		//確率1-Ant.bestCellNextでこっちを実行する.
+		//食べ物フェロモンの強さに応じた確率で移動先の隣接セルを決定してそこに移動する.
+		else{ //give cells chance based on pheremone
+			double pheremonesSoFar = 0;
+			double goalPheromoneLevel = totalNeighborPheromones * Math.random();
+			//全ての隣接セルについて
+			for(Cell neighbor : allNeighborCells){
+				//環境中に食べ物セルがない場合
+				if(ants.getFood().isEmpty()){
+					pheremonesSoFar += 1;
+					if(pheremonesSoFar > goalPheromoneLevel){
+
+						x = neighbor.c;
+						y = neighbor.r;
+						break;
+					}
 				}
-			}	
-			//確率1-Ant.bestCellNextでこっちを実行する.
-			//食べ物フェロモンの強さに応じた確率で移動先の隣接セルを決定してそこに移動する.
-			else{ //give cells chance based on pheremone
-				double pheremonesSoFar = 0;
-				double goalPheromoneLevel = totalNeighborPheromones * Math.random();
-				//全ての隣接セルについて
-				for(Cell neighbor : allNeighborCells){
-					//環境中に食べ物セルがない場合
-					if(ants.getFood().isEmpty()){
-						pheremonesSoFar += 1;
+				//環境中に食べ物セルがある場合
+				else{
+
+					for(Cell food : ants.getFood()){
+						//発見済みの食べ物のフェロモンは無視する.
+						if(foodFound.contains(food)){
+							continue;
+						}
+						pheremonesSoFar+=neighbor.getFoodPheromoneLevel(food);
 						if(pheremonesSoFar > goalPheromoneLevel){
 
 							x = neighbor.c;
 							y = neighbor.r;
-							break;
-						}
-					}
-					//環境中に食べ物セルがある場合
-					else{
-
-						for(Cell food : ants.getFood()){
-							//発見済みの食べ物のフェロモンは無視する.
-							if(foodFound.contains(food)){
-								continue;
-							}
-							pheremonesSoFar+=neighbor.getFoodPheromoneLevel(food);
-							if(pheremonesSoFar > goalPheromoneLevel){
-
-								x = neighbor.c;
-								y = neighbor.r;
-								return;
-							}
+							return;
 						}
 					}
 				}
 			}
-		//}
+		}
 	}
 
 	//その蟻が何列目にいるか
@@ -223,11 +216,4 @@ public class Ant {
 	public int getRow(){
 		return y;
 	}
-
-	//その蟻が巣に帰ろうとしているかどうか(最終的にこの関数も要らなくなる.)
-	/*
-	public boolean isReturningHome() {
-		return returnToNest;
-	}
-	*/
 }
