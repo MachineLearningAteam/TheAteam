8ac0d9c76c1f41fdf5e738ca213cd32b2407558b
fujimurakengo <naruawaryousuke@gmail.com>
2018-06-23 15:00:03 +0900

    int型をint[][]に直した

--- src/Imagefileload.java
+++ src/Imagefileload.java
@@ -4,39 +4,37 @@ import java.io.*;
 import javax.imageio.ImageIO;
 
 public class Imagefileload{
-    public int[][] get_feald(){
-   int[][] feald;
-   feald = new int[100][100];
-    String string = "map1.bmp";
-    try {
-		BufferedImage img = ImageIO.read(new File(string));
+    int[][] get_feald(){
+        int[][] feald;
+        feald = new int[100][100];
+        String string = "map1.bmp";
+        try {
+		    BufferedImage img = ImageIO.read(new File(string));
       //  FileInputStream fis = new FileInputStream("map1.bmp");
-        System.out.println("Width:"  + img.getWidth());
-        System.out.println("Height:" + img.getHeight());
+            System.out.println("Width:"  + img.getWidth());
+            System.out.println("Height:" + img.getHeight());
       //  System.out.println(img);
-        for(int i=0;i<100;i++){
-            for(int j=0;j<100;j++){
-                int data =img.getRGB(j, i);
-                if (data==-1){
-                    //System.out.print("1"); 
-                    feald[i][j]  =1;
-                  }
-                else{
+            for(int i=0;i<100;i++){
+                for(int j=0;j<100;j++){
+                    int data =img.getRGB(j, i);
+                    if (data==-1){
+                        //System.out.print("1"); 
+                        feald[i][j]  =1;
+                    }
+                    else{
                 //System.out.print("0"); 
-                    feald[i][j]  =0;
+                        feald[i][j]  =0;
+                    }
                 }
-
+            System.out.println();
             }
-        System.out.println();
-        }
-    } catch ( IOException e) { 
+        } catch ( IOException e) { 
         
-        System.err.println("no date");
+            System.err.println("no date");
         //return 0;
        // System.exit()
-      }
+        }
        // fis.close();
-    int[][] feald2 = feald;
-	return feald2;
+	    return feald;
     }
 }
